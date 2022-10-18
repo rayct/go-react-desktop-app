d0372af1d18310c95be20bea1b5f4b1cef2bdbb0
rayct <rayct@icloud.com>
2022-10-18 02:00:22 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+//go:embed build/appicon.png
+var icon []byte
+
+func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:            "go-react-desktop-app",
+		Width:            1024,
+		Height:           768,
+		Assets:           assets,
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+		// Below is an example of additional configuration settings for macOS builds //
+		Mac: &mac.Options{
+			TitleBar: &mac.TitleBar{
+				TitlebarAppearsTransparent: false,
+				HideTitle:                  false,
+				HideTitleBar:               false,
+				FullSizeContent:            true,
+				UseToolbar:                 false,
+				HideToolbarSeparator:       true,
+			},
+			About: &mac.AboutInfo{
+				Title:   "My Project",
+				Message: "An example Wails project for Raymond Turemner's Portfolio",
+				Icon:    icon,
+			},
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
